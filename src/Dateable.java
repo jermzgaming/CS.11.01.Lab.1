e3f798e5e11abdc8660d5e1cebcdac2013ae498a
n30daneo <neoyeanlaic2027@student.cis.edu.hk>
2024-08-28 10:08:18 +0800

    Neo Lab 1 Complete

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int age = -1;
+        while (age < 0) {
+            try {
+                age = Integer.parseInt(scanner.nextLine());
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Please enter a valid number!");
+            }
+        }
+        int minAge = 7+(age/2);
+        System.out.println(age + "-year olds should date somebody who is at least " + minAge + " years old.");
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int age = -1;
+        while (age < 0) {
+            try {
+                age = Integer.parseInt(scanner.nextLine());
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Please enter a valid number!");
+            }
+        }
+        int minPages = 100-age;
+        System.out.println(age + "-year olds should read at least " + minPages + " pages before giving up on a book.");
     }
 }
